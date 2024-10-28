3ba5045fc93e41bfaefa161c3b932e2844dc1840
unknown <you@example.com>
2024-10-28 18:52:24 +0900

    o'zgarishlarni saqlash

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,1 @@
 console.log("EXECUTED");
-
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime)
-
-const person: string = "Martin";
-const count: number = 100;
