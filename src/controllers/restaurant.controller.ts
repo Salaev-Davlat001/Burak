05adbf4d6cb4fa2399dcf8930aa874a764ac9b0d
unknown <you@example.com>
2024-11-08 17:38:25 +0900

    feat: create restaurant member related api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -22,9 +22,28 @@ restaurantController.getLogin =  (req: Request, res: Response) => {
 
 restaurantController.getSignup =  (req: Request, res: Response) => {
     try {
+        console.log("sign up")
         res.send("Signup Page")  
     }catch(err) {
         console.log("Error, getSignup:", err);
+    }
+};
+
+restaurantController.processLogin =  (req: Request, res: Response) => {
+    try {
+        console.log("processLogin")  
+         res.send("DONE")
+    }catch(err) {
+        console.log("Error, processLogin:", err);
+    }
+};
+
+restaurantController.processSignup =  (req: Request, res: Response) => {
+    try {
+        console.log("processSignup")  
+         res.send("DONE")
+    }catch(err) {
+        console.log("Error, processSignup:", err);
     }
 };
 
