50ca7bc9e7eedf233913bbd640f74f06cfab729d
unknown <you@example.com>
2024-11-26 09:05:09 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./routerAdmin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 /**1-ENTRANCE**/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +22,17 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /**2-SESSION**/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+    store: store,
+    resave: true, //user kirgan vaqtga yana yangi vaqtni qo'shib save qilish buyrug'i
+    saveUninitialized: true,
+  })
+);
 
 /**3-VIEWS**/
 app.set("views", path.join(__dirname, "views"));
