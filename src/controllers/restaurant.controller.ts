13a5a67244f5a593d6d85ac63a8b1049a50b8515
unknown <you@example.com>
2024-12-06 11:42:05 +0900

    feat: develop updateChosenUser api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -104,7 +104,7 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   try {
     console.log("getUser");
     const result = await memberService.getUsers();
-    console.log("result", result);
+
     res.render("users", { users: result });
   } catch (err) {
     console.log("Error, getUsers:", err);
@@ -112,11 +112,16 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
   }
 };
 
-restaurantController.updateChosenUser = (req: Request, res: Response) => {
+restaurantController.updateChosenUser = async (req: Request, res: Response) => {
   try {
     console.log("updateChosenUser");
+    const result = await memberService.updateChosenUser(req.body);
+
+    res.status(HttpCode.OK).json({ data: result });
   } catch (err) {
-    console.log("Error, updateChosenUser:", err);
+    console.log("Error, updateChosenuser:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
   }
 };
 
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,8 +1,14 @@
 import MemberModel from "../schema/Member.model";
-import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import {
+  LoginInput,
+  Member,
+  MemberInput,
+  MemberUpdateInput,
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
+import { shapeintroMongooseObjectId } from "../libs/config";
 
 class MemberService {
   private readonly memberModel;
@@ -92,6 +98,15 @@ class MemberService {
       .find({ memberType: MemberType.USER })
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+
+  public async updateChosenUser(input: MemberUpdateInput): Promise<any> {
+    input._id = shapeintroMongooseObjectId(input._id);
+    const result = await this.memberModel
+      .findByIdAndUpdate({ _id: input._id }, input, { new: true })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
     return result;
   }
 }
