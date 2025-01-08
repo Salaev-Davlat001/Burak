51dc7f76b2f33eb611621716e0cb064426f46684
unknown <you@example.com>
2025-01-08 13:58:51 +0900

    feat: develop token creation via jwt

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /**4-ROUTERS**/
-app.use("/admin", routerAdmin);
-app.use("/", router);
+app.use("/admin", routerAdmin); //BSSR
+app.use("/", router); //SPA
 export default app;
--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -14,7 +14,9 @@ export enum Message {
   NO_DATA_FOUND = "No data is found!",
   CREATE_FAILED = "Create is failed!",
   UPDATE_FAILED = "Update is failed!",
+
   BLOCKED_USER = "You have been blocked, contackt restaurant!",
+  TOKEN_CREATION_FAILED = "Token creation error!",
   NO_MEMBER_NICK = "No member with that nickname! ",
   USED_NICK_PHONE = "You are inserting already used phone!",
   WRONG_PASSWORD = "Wrong password, please try again!",
